179958ca052269b4a67e22dba12b76049907ea45
djo <garnotel.g@sfeir.com>
2017-04-27 11:45:51 +0200

    dataflow project

---
+++ dataflow/gcptraining/src/main/java/com/sfeir/gcptraining/WordCountFile.java
@@ -0,0 +1,94 @@
+package com.sfeir.gcptraining;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.options.BigQueryOptions;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.DefaultValueFactory;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.Validation.Required;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+
+import com.sfeir.gcptraining.WordCount.CountWords;
+import com.sfeir.gcptraining.WordCount.FormatAsTableRow;
+import com.sfeir.gcptraining.WordCount.FormatAsTextFn;
+
+public class WordCountFile {
+
+	public interface WordCountOptions extends PipelineOptions, BigQueryOptions {
+	
+		    /**
+	     * By default, it's bucket sample
+	     */
+	    @Description("Path of the file to read from")
+	    @Default.String("gs://apache-beam-samples/")
+	    String getBucketFile();
+	    void setBucketFile(String value);
+	
+	    /**
+	     * By default, this example reads from a public dataset containing the text of
+	     * King Lear. Set this option to choose a different input file or glob.
+	     */
+	    @Description("Path of the file to read from")
+	    @Default.String("shakespeare/kinglear.txt")
+	    String getInputFile();
+	    void setInputFile(String value);
+	
+	    /**
+	     * Set this required option to specify where to write the output.
+	     */
+	    @Description("Path of the file to write to")
+	    @Required
+	    String getOutput();
+	    void setOutput(String value);
+	
+	    /**
+	     * Set this required option to specify where to write the output.
+	     */
+	    @Description("Table for BigQuery : dataset.table")
+	    @Required
+	    String getTableBQ();
+	    void setTableBQ(String value);  
+	
+	    @Description("Table for BigQuery : dataset.table")
+		@Default.InstanceFactory(TableNameBQFactory.class)
+	    @Required
+	    String getTableCompleteBQ();
+	    void setTableCompleteBQ(String value);  
+	
+	    static class TableNameBQFactory implements DefaultValueFactory<String> {
+			@Override
+			public String create(PipelineOptions options) {
+				WordCountOptions woptions = options.as(WordCountOptions.class);
+				return woptions.getProject() + ":" + woptions.getTableBQ();
+			}
+	    }
+	
+	}
+
+	  public static void main(String[] args) {
+		    WordCountOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
+		      .as(WordCountOptions.class);
+		    Pipeline p = Pipeline.create(options);
+
+		    PCollection<KV<String, Long>> counts = p.apply("ReadLines Files", TextIO.Read.from(options.getInputFile()))
+		    										.apply(new CountWords());
+		     
+		     counts.apply(MapElements.via(new FormatAsTextFn()))
+		     	   .apply("WriteCounts", TextIO.Write.to(options.getOutput()));
+
+		     counts.apply(MapElements.via(new FormatAsTableRow()))
+			   	   .apply("WriteCounts in BigQuery", 
+			   			 BigQueryIO.Write
+				   	        .to(options.getTableCompleteBQ())
+				   	        .withSchema(WordCount.buildSchema())
+				   	        .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+				   	        .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE));
+		    p.run().waitUntilFinish();
+		  }
+}
---
+++ dataflow/gcptraining/src/main/java/com/sfeir/gcptraining/WordCountStream.java
@@ -0,0 +1,99 @@
+package com.sfeir.gcptraining;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.io.PubsubIO;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.DefaultValueFactory;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.PubsubOptions;
+import org.apache.beam.sdk.options.Validation.Required;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.joda.time.Duration;
+
+import com.sfeir.gcptraining.WordCount.CountWords;
+import com.sfeir.gcptraining.WordCount.FormatAsTableRow;
+import com.sfeir.gcptraining.WordCount.FormatAsTextFn;
+
+public class WordCountStream {
+	  public interface WordCountOptions extends PubsubOptions {
+
+		    /**
+		     * Set this required option to specify where to write the output.
+		     */
+		    @Description("Path of the file to write to")
+		    @Required
+		    String getOutput();
+		    void setOutput(String value);
+
+		    /**
+		     * Set this required option to specify where to write the output.
+		     */
+		    @Description("Table for BigQuery : dataset.table")
+		    @Required
+		    String getTableBQ();
+		    void setTableBQ(String value);  
+
+		    /**
+		     * Set this required option to specify where to write the output.
+		     */
+		    @Description("Topic for pubsub")
+		    @Default.String("mytopic")
+		    String getTopic();
+		    void setTopic(String value);  
+
+		    @Description("Table for BigQuery : dataset.table")
+			@Default.InstanceFactory(TableNameBQFactory.class)
+		    @Required
+		    String getTableCompleteBQ();
+		    void setTableCompleteBQ(String value);  
+
+		    @Description("Topic for pubsub")
+			@Default.InstanceFactory(PubSubFactory.class)
+		    String getTopicComplete();
+		    void setTopicComplete(String value);  
+
+		    static class TableNameBQFactory implements DefaultValueFactory<String> {
+				@Override
+				public String create(PipelineOptions options) {
+					WordCountOptions woptions = options.as(WordCountOptions.class);
+					return woptions.getProject() + ":" + woptions.getTableBQ();
+				}
+		    }
+
+		    static class PubSubFactory implements DefaultValueFactory<String> {
+				@Override
+				public String create(PipelineOptions options) {
+					WordCountOptions woptions = options.as(WordCountOptions.class);
+					return "projects/" + woptions.getProject() + "/topics/" + woptions.getTopic();
+				}
+		    }
+		  }
+	  
+	  public static void main(String[] args) {
+		    WordCountOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
+		      .as(WordCountOptions.class);
+		    Pipeline p = Pipeline.create(options);
+
+		    PCollection<KV<String, Long>> counts = p.apply("ReadLines PubSub", PubsubIO.<String>read().topic(options.getTopicComplete()).withCoder(StringUtf8Coder.of()))
+										    		.apply(Window.<String>into(
+											    		    FixedWindows.of(Duration.standardMinutes(1))))
+		    										.apply(new CountWords());
+
+		     counts.apply(MapElements.via(new FormatAsTableRow()))
+			   	   .apply("WriteCounts in BigQuery", 
+			   			 BigQueryIO.Write
+				   	        .to(options.getTableCompleteBQ())
+				   	        .withSchema(WordCount.buildSchema())
+				   	        .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
+		    p.run().waitUntilFinish();
+		  }
+}
